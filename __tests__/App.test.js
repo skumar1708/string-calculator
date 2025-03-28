26de9866478b3c18fee53258af0111ed1cc0dedb
skumar1708 <itbhu.sk@gmail.com>
2025-03-28 13:21:39 +0530

    Implement the add Function Step by Step

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,4 +1,4 @@
-import { add } from "./stringCalculator";
+import { add } from "../src/utils/stringCalculator";
 
 describe("String Calculator test case", () => {
   test("should return 0 for an empty string", () => {
---
+++ src/utils/stringCalculator.js
@@ -0,0 +1,25 @@
+function add(numbers) {
+    if (!numbers) return 0;
+  
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(parts[0].slice(2));
+      numbers = parts[1];
+    }
+  
+    const numArray = numbers
+      .split(delimiter)
+      .map((num) => parseInt(num, 10))
+      .filter((num) => !isNaN(num));
+  
+    const negatives = numArray.filter((num) => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+    }
+  
+    return numArray.reduce((sum, num) => sum + num, 0);
+  }
+  
+  module.exports = { add };
+  
