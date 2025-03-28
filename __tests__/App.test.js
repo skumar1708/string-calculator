5d343726d8fbbc76fd4ae731737b35a5953f9025
skumar1708 <itbhu.sk@gmail.com>
2025-03-28 14:46:15 +0530

    Added test case for UI rendering \n Added jest libery

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import App from "../src/App";
 import { add } from "../src/utils/stringCalculator";
 
 describe("String Calculator test case", () => {
@@ -5,3 +9,13 @@ describe("String Calculator test case", () => {
     expect(add("")).toBe(0);
   });
 });
+
+describe("String calculator UI test cases", () => {
+  test("renders input field and button", () => {
+    render(<App />);
+    expect(screen.getByPlaceholderText("Enter numbers")).toBeInTheDocument();
+    expect(screen.getByText("Calculate")).toBeInTheDocument();
+  });
+});
+
+
